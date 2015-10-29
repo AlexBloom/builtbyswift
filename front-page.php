3d06bd559df66ead20a89733064b82e24440281c
Jason Britton <velograph@gmail.com>
2015-10-28 22:56:51 -0700

    Fix thumbs on homepage

--- front-page.php
+++ front-page.php
@@ -188,7 +188,7 @@ get_header(); ?>
 
 								<div class="product-portal">
 									<a href="<?php the_permalink(); ?>">
-										<?php the_post_thumbnail('thumbnail'); ?>
+										<?php the_post_thumbnail('thumb'); ?>
 										<h3><?php the_title(); ?></h3>
 									</a>
 								</div>
