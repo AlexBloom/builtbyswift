36d36cc04494bed1c3cc9a9d7c0484cbc28aedea
Jason Britton <velograph@gmail.com>
2016-03-03 11:45:24 -0800

    Update woo templates

--- woocommerce/emails/admin-failed-order.php
+++ woocommerce/emails/admin-failed-order.php
@@ -1,17 +1,17 @@
 <?php
 /**
- * Admin new order email
+ * Admin failed order email
  *
- * This template can be overridden by copying it to yourtheme/woocommerce/emails/admin-new-order.php.
+ * This template can be overridden by copying it to yourtheme/woocommerce/emails/admin-failed-order.php
  *
- * HOWEVER, on occasion WooCommerce will need to update template files and you (the theme developer).
- * will need to copy the new files to your theme to maintain compatibility. We try to do this.
- * as little as possible, but it does happen. When this occurs the version of the template file will.
+ * HOWEVER, on occasion WooCommerce will need to update template files and you (the theme developer)
+ * will need to copy the new files to your theme to maintain compatibility. We try to do this
+ * as little as possible, but it does happen. When this occurs the version of the template file will
  * be bumped and the readme will list any important changes.
  *
  * @see 	    http://docs.woothemes.com/document/template-structure/
- * @author WooThemes
- * @package WooCommerce/Templates/Emails/HTML
+ * @author  WooThemes
+ * @package WooCommerce/Templates/Emails
  * @version 2.5.0
  */
 
@@ -24,7 +24,7 @@
   */
  do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
 
- <p><?php printf( __( 'You have received an order from %s. The order is as follows:', 'woocommerce' ), $order->get_formatted_billing_full_name() ); ?></p>
+ <p><?php printf( __( 'Payment for order #%d from %s has failed. The order was as follows:', 'woocommerce' ), $order->get_order_number(), $order->get_formatted_billing_full_name() ); ?></p>
 
  <?php
 
